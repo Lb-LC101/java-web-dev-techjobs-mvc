30355a1b120a567ec06d91a5ce34ff7df3972c1f
Lb-LC101 <lisabartfeld@gmail.com>
2021-02-21 10:33:18 -0600

    created TechJobsController and getActionChoices method, updated fragments

---
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/TechJobsController.java
@@ -0,0 +1,23 @@
+package org.launchcode.javawebdevtechjobsmvc.controllers;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+
+
+public class TechJobsController {
+    @ModelAttribute("actions")
+    public static HashMap<String, String> getActionChoices(Model model) {
+
+        HashMap<String, String> actionChoices = new HashMap<>();
+        actionChoices.put("search", "Search");
+        actionChoices.put("list", "List");
+
+        model.addAttribute("actions", actionChoices);
+        return actionChoices;
+    }
+
+
+}
